2d8e9ee078721df5bdf04b9c0bd47c5ff4de9237
tyodev <deve@tyo.com.au>
2017-05-21 11:07:19 +1000

    works as expected now to generate field/member definitions

--- java.js
+++ java.js
@@ -4,29 +4,47 @@
  * Take json as input and convert the keys into fields
  */
 
-fs = require('fs')
-log = console; // require('./log');
+var fs = require('fs'),
+    util = require('util');
+
+var log = console; // require('./log');
+var params = require('./params');
 var jsonFile = process.argv[2];
 var jsonString = fs.readFileSync(jsonFile, 'utf8').trim();
+var datatype = require('tyo-utils').dataype;
+var javatype = require('./lib/java/type');
 
 // for being lazy, if the input is not started / ended with barackets {} , [], it will be treated as an object
 var firstC, lastC;
-fristC = jsonString[0];
+firstC = jsonString.charAt(0);
 lastC = jsonString[jsonString.length - 1];
-jsonString = ((firstC !== '{' && firstC !== '[') ? '{' : '')
-            + jsonString
-            ((lastC !== '}' && lastC !== ']') ? '}' : '');
+jsonString = ((firstC !== '{' && firstC !== '[') ? '{' : '') + jsonString + ((lastC !== '}' && lastC !== ']') ? '}' : '');
+
+var longOpts = params['--'];
+var shortOpts = params['-'];
+
+var jsonTemplate = "%";
+if (longOpts.class)
+    jsonTemplate = "public class " + longOpts.class + `{
+        %s
+        }`;
 
 var jsonObj;
 try {
     jsonObj = JSON.parse(jsonString);
-
-    for (var key in jsonObj)
-        console.log('public ')
+    var fieldsOut = "";
+    for (var key in jsonObj) {
+        var value = jsonObj[key];
+        var type = javatype.toJavaTypeString(value);
+        var fieldOut = 'public ' + type + ' ' + key + ';';
+        fieldsOut += fieldOut + '\n';
+    }
+    console.log(fieldsOut);
 }
 catch (e) {
     jsonObj = {};
-    log.error('Parsing json file error', e);
+    log.error('Parsing json file error');
+    log.error(e);
     process.exit(-1);
 }
 
