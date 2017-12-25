6c3f01beda032db1ffee3f1a47a8483860e6614b
tyodev <deve@tyo.com.au>
2017-12-25 11:57:52 +1000

    create a separate function convert json value type to java

--- java.js
+++ java.js
@@ -10,16 +10,11 @@ var fs = require('fs'),
 var log = console; // require('./log');
 var Params = require('./params');
 var optsAvailable = {
-    'key-before-all': false, 
-    prefix: {desc: '"a-prefix-string"', default: "public ", short: 'p', input: null}, 
-    suffix: {desc: '"a-suffix-string"', default: ";", short: 's', input: null}, 
-    "to-java-type": null, 
-    "text-before-key": {desc: '"the text before the key"' , default: "", input: null},
-    camelcase: true, 
-    class: false, 
-    serialize: false,
-    "key-to-all-caps": false,
-    format: {desc: "the-format-of-output", default: null, input: null}
+    //
+    'from-json-to-class': true,
+
+    // 
+    'json-to-type': false
 };
 
 var params = new Params(optsAvailable);
@@ -44,75 +39,14 @@ var inputs = opts['---'];
 var jsonFile = Array.isArray(inputs) ? inputs[0] : inputs;
 var jsonString = fs.readFileSync(jsonFile, 'utf8').trim();
 var datatype = require('tyo-utils').dataype;
-var javatype = require('./lib/java/type');
+var java = require('./lib/java').java;
 
-// for being lazy, if the input is not started / ended with barackets {} , [], it will be treated as an object
-var firstC, lastC;
-firstC = jsonString.charAt(0);
-lastC = jsonString[jsonString.length - 1];
-jsonString = ((firstC !== '{' && firstC !== '[') ? '{' : '') + jsonString + ((lastC !== '}' && lastC !== ']') ? '}' : '');
+if (longOpts['from-json-to-class']) {
 
-var jsonTemplate = "%";
-if (longOpts.class)
-    jsonTemplate = "public class " + longOpts.class + `{
-        %s
-        }`;
-
-var jsonObj;
-try {
-    jsonObj = JSON.parse(jsonString);
-    var fieldsOut = "";
-    var prefix = longOpts.prefix; // || shortOpts.p || optsAvailable.prefix.default;
-    var suffix = longOpts.suffix; // || shortOpts.s || optsAvailable.suffix.default;
-    var textBeforeText = longOpts["text-before-key"] || optsAvailable["text-before-key"].default;
-    for (var key in jsonObj) {
-        var value = jsonObj[key];
-
-        var targetKey = key;
-        if (!!longOpts["key-to-all-caps"])
-            targetKey = key.toUpperCase();
-
-        var type = "";
-        var fieldOut = "";
-        if (!!longOpts["to-java-type"]) {
-            type = javatype.toJavaTypeString(value, {serialize: !!longOpts.serialize});
-            
-            if (!!longOpts.camelcase) {
-                type = type.charAt(0).toUpperCase() + type.substr(1);
-            }
-
-            if (datatype.isString(longOpts["to-java-type"]))
-                type += longOpts["to-java-type"];
-            else
-                type += ' ';
-        }
-        else {
-            //
-        }
-
-        if (longOpts.format === null || longOpts.format.length > 0) {
-            if (longOpts['key-before-all'])
-                fieldOut = textBeforeText + targetKey + prefix + type + suffix;
-            else
-                fieldOut = prefix + type + textBeforeText + targetKey + suffix;
-        }
-        else {
-            var replaceKey = longOpts.format.replaceAll("\\$key", key);
-            if (replaceKey !== null)
-                replaceKey = replaceKey.replaceAll("\\$KEY", targetKey);
-
-            if (replaceKey !== null)
-                fieldOut = replaceKey.replaceAll("\\$type", type);
-        }
-
-        fieldsOut += fieldOut + '\n';
-    }
-    console.log(fieldsOut);
 }
-catch (e) {
-    jsonObj = {};
-    log.error('Parsing json file error');
-    log.error(e);
-    process.exit(-1);
+else if (longOpts['json-to-type']) {
+    var target = java.jsonToType(jsonString);
+
+    console.log(JSON.stringify(target));
 }
 
