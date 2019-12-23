f14bef80f7df920060528e084e8cb0321ba3504a
Eric Tang <eric.tang@tyo.com.au>
2019-12-23 11:13:16 +1000

    added the default value check (object)

--- params.js
+++ params.js
@@ -10,14 +10,14 @@ function logError(message) {
 }
 
 /**
- * 
- * @param {*} defaults 
+ *
+ * @param {*} defaults
  * @param {*} enforceEmptyOption true/false, to check the option exists in the default option, if not an exception will be thrown
  */
 
 function Params(defaults, enforceEmptyOption) {
     this.defaults = defaults || {};
-    this.enforceEmptyOption = enforceEmptyOption; 
+    this.enforceEmptyOption = enforceEmptyOption;
 
     this.optCount = -1;
     /**
@@ -25,7 +25,7 @@ function Params(defaults, enforceEmptyOption) {
      */
     this.params = {};
     this.argv = null;
-    
+
     this['-'] = {};
 
     // { name: {desc: "", default: value, sample: value}}
@@ -54,17 +54,17 @@ Params.prototype.append = function (obj, value) {
 }
 
 /**
- * 
+ *
  */
 
 Params.prototype.getOpts = function (argv) {
     this.argv = argv;
-    
+
     return this.parse();
 }
 
 /**
- * 
+ *
  */
 
 Params.prototype.parse = function () {
@@ -73,126 +73,144 @@ Params.prototype.parse = function () {
         return this.params;
 
     this.optCount = 0;
-    this.argv = this.argv || process.argv;
-    
+    if (!this.argv) {
+        this.argv = process.argv;
+        this.argv.shift(); // the node itself
+        this.argv.shift(); // the node script / program
+    }
+
     var params = this.params;
-    var param = 2;
-    if (this.argv.length > 2) {
-        for (; param < this.argv.length; ++param) {
-            this.optCount += 1;
+    var param = 0;
+    for (; param < this.argv.length; ++param) {
+        this.optCount += 1;
 
-            var paramStr = this.argv[param];
-            var o = paramStr.charAt(0);
-            if (o === '-') {
-                if (paramStr.length > 1) {
-                    var c = paramStr.charAt(1);
-                    var isEmptyOption = false;
-                    /**
-                     * Get the long key
-                     */
-                    var pos = 1;
-                    if (c == '-')
-                        pos = 2;
-                    
-                    var key = paramStr.substr(pos);
-                    var longKey;
-                    if (pos === 2) {
+        var paramStr = this.argv[param];
+        var o = paramStr.charAt(0);
+        if (o === '-') {
+            if (paramStr.length > 1) {
+                var c = paramStr.charAt(1);
+                var isEmptyOption = false;
+                /**
+                 * Get the long key
+                 */
+                var pos = 1;
+                if (c == '-')
+                    pos = 2;
+
+                var key = paramStr.substr(pos);
+                var longKey;
+                if (pos === 2) {
+                    longKey = key;
+                } else {
+                    longKey = this['-'][key];
+                    if (!longKey)
                         longKey = key;
-                    }
-                    else {
-                        longKey = this['-'][key];
-                        if (!longKey)
-                            longKey = key;
-                    }
-                    isEmptyOption = !(longKey in params);
-                    if (isEmptyOption && this.enforceEmptyOption) {
-                        console.error('Unknown option "' + longKey + '", please check your input and try again');
-                        process.exit(-1);
-                    }
-
-                    /**
-                     * The option doesn't have to have a value
-                     */
-                    params[key] = null;
-
-                    /**
-                     * Now the value of the option
-                     */
-                    var nextParam = (param + 1);
-                    var nextValue = null;
-                    /**
-                     * Get the option value
-                     * 
-                     * 1) 
-                     * if inputs.length > 0
-                     * 
-                     * the next value should be paramter value
-                     * 
-                     * 2) OK, 
-                     * 
-                     */
-                    if (nextParam < this.argv.length ) {
-
-                        // if the next param is a value
-                        if (this.argv[nextParam].charAt(0) !== '-') {
-                            // we can only know what to do is that we know the default value / type of an option
-                            if (!isEmptyOption) {
-                                nextValue = this.argv[nextParam];
-
-                                if (nextValue === 'true') 
-                                    nextValue = true;
-                                else if (nextValue === 'false')
-                                    nextValue = false;
-                                else {
-                                    // a non boolean value is provided for the option
-                                    if (params[key] === true || params[key] === false) {
-                                        // not supposed to take any value for this option
-                                        // if the option exist, the default boolean value is true
-                                        logError('A boolean value is needed for option: "' + longKey + '", please check your input and try it again');
-                                    }
-                                }
-
-                                if ((nextValue === true || nextValue == false) &&
-                                    (params[key] !== true && params[key] !== false)) {
-                                    logError("A non boolean value is required for options: " + longKey);
-                                }
-                                
-                                param = nextParam;
-
-                                /**
-                                 * @todo data type check
-                                 */
-                                params[key] = this.append(params[key], nextValue);
-                            }
-                        }
-                        // no value provided
-                        else {
-                            // if the option (with the key) doesn't not exist in the default option
-                            // we won't be able to tell if it should be followed by a value or if it is a switch (true/false)
-                            // so we just ignore it
-                            if (!isEmptyOption) {
-                                // if the default value isn't a boolean value
-                                if (params[key] !== true && params[key] !== false) {
-                                    console.error('A value needs to be provided for option: "' + longKey + '", please check your input and try again');
-                                    process.exit(-1);
-                                }
-                                // else
-                                // we will set it true as we set in the option
-                                else {
-                                    params[key] = true;
-                                }
-                            }
-                        }
-                    }
-            
                 }
-                else 
-                    // in case, there are also needs for output and error
-                    params['----'] = {input: process.stdin, output: null, error: null};
-            }
-            else
-                params['---'] = this.append(params['---'], paramStr);
-        }
+                isEmptyOption = !(longKey in params);
+                if (isEmptyOption && this.enforceEmptyOption) {
+                    console.error('Unknown option "' + longKey + '", please check your input and try again');
+                    process.exit(-1);
+                }
+
+                /**
+                 * The option doesn't have to have a value
+                 */
+                var defaultValue = null;
+                var nullable = true;
+                if (this.defaults[key]) {
+                    if (typeof this.defaults[key] === 'object') {
+                        // Get the default value
+                        defaultValue = (this.defaults[key].default) ? this.defaults[key].default : null;
+
+                        // Get if it is nullable
+                        if (typeof this.defaults[key].nullable === 'boolean')
+                            nullable = (this.defaults[key].nullable);
+                        else
+                            throw new Error("The nullability of a default value must be boolean type");
+                    } else
+                        defaultValue = this.defaults[key];
+                }
+                params[key] = defaultValue;
+
+                /**
+                 * Now the value of the option
+                 */
+                var nextParam = (param + 1);
+                var nextValue = null;
+                /**
+                 * Get the option value
+                 *
+                 * 1)
+                 * if inputs.length > 0
+                 *
+                 * the next value should be paramter value
+                 *
+                 * 2) OK,
+                 *
+                 */
+                if (nextParam < this.argv.length) {
+
+                    // if the next param is a value
+                    if (this.argv[nextParam].charAt(0) !== '-') {
+                        // we can only know what to do is that we know the default value / type of an option
+                        if (!isEmptyOption) {
+                            nextValue = this.argv[nextParam];
+
+                            if (nextValue === 'true')
+                                nextValue = true;
+                            else if (nextValue === 'false')
+                                nextValue = false;
+                            else {
+                                // a non boolean value is provided for the option
+                                if (params[key] === true || params[key] === false) {
+                                    // not supposed to take any value for this option
+                                    // if the option exist, the default boolean value is true
+                                    logError('A boolean value is needed for option: "' + longKey + '", please check your input and try it again');
+                                }
+                            }
+
+                            if ((nextValue === true || nextValue == false) &&
+                                (params[key] !== true && params[key] !== false)) {
+                                logError("A non boolean value is required for options: " + longKey);
+                            }
+
+                            param = nextParam;
+
+                            /**
+                             * @todo data type check
+                             */
+                            params[key] = this.append(params[key], nextValue);
+                        }
+                    }
+                    // no value provided
+                    else {
+                        // if the option (with the key) doesn't not exist in the default option
+                        // we won't be able to tell if it should be followed by a value or if it is a switch (true/false)
+                        // so we just ignore it
+                        if (!nullable || !isEmptyOption) {
+                            // if the default value isn't a boolean value
+                            if (params[key] !== true && params[key] !== false) {
+                                console.error('A value needs to be provided for option: "' + longKey + '", please check your input and try again');
+                                process.exit(-1);
+                            }
+                            // else
+                            // we will set it true as we set in the option
+                            else {
+                                params[key] = true;
+                            }
+                        }
+                    }
+                }
+
+            } else
+                // in case, there are also needs for output and error
+                params['----'] = {
+                    input: process.stdin,
+                    output: null,
+                    error: null
+                };
+        } else
+            params['---'] = this.append(params['---'], paramStr);
     }
 
     return this.params;
@@ -210,7 +228,7 @@ Params.prototype.getOptCount = function () {
 }
 
 /**
- * 
+ *
  */
 
 Params.newInstance = function (defaults) {
@@ -218,7 +236,7 @@ Params.newInstance = function (defaults) {
 }
 
 /**
- * 
+ *
  */
 
 Params.prototype.setUsage = function (opts) {
@@ -228,7 +246,7 @@ Params.prototype.setUsage = function (opts) {
 }
 
 /**
- * 
+ *
  */
 
 Params.prototype.parseUsage = function () {
@@ -244,11 +262,9 @@ Params.prototype.parseUsage = function () {
                 if (obj.short) {
                     this['-'][obj.short] = key;
                 }
-            }
-            else
+            } else
                 this.params[key] = obj;
-        }
-        else
+        } else
             this.params[key] = null;
     }
 }
@@ -270,7 +286,7 @@ Params.prototype.showUsage = function (filename) {
             switchStr = "-" + key;
         else
             switchStr = "--" + key;
-        
+
         optStr += switchStr;
 
         var objValue = this.opts[key];
@@ -280,10 +296,12 @@ Params.prototype.showUsage = function (filename) {
                 objValue.switch = switchStr;
                 if (objValue.sample !== null)
                     value = objValue.sample;
-            }
-            else {
+            } else {
                 value = objValue;
-                var obj = {switch: switchStr, default: objValue};
+                var obj = {
+                    switch: switchStr,
+                    default: objValue
+                };
                 this.opts[key] = obj;
             }
         }
@@ -297,8 +315,8 @@ Params.prototype.showUsage = function (filename) {
     for (var key in this.opts) {
         var objValue = this.opts[key];
         if (objValue && objValue.desc) {
-            console.error('\t\t' + objValue.switch + '\t\t' + objValue.desc);
-            console.error('\t\t\t\t' + '\t' + 'default: "' + objValue.default + '"');
+            console.error('\t\t' + objValue.switch+'\t\t' + objValue.desc);
+            console.error('\t\t\t\t' + '\t' + 'default: "' + objValue.default+'"');
             console.error();
         }
     }
